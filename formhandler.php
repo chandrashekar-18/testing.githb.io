7fbdcebb4fd8ed361384c92fc8a832e1fe30cf82
Chandrashekar <88418122+chandrashekar-18@users.noreply.github.com>
2021-09-16 17:25:29 +0530

    nothing

--- formhandler.php
+++ formhandler.php
@@ -1,27 +1,27 @@
 <?php
-$name = $_POST['name'];
+$name =$_POST['name'];
 $visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
+$subject =$_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'myname@gmail.com'
 
-$email_subject = 'New Form Submission'
+$email_form = '@info@nikhilwebsite.com';
+
+$email_subject = ' New Form Submission';
 
 $email_body = "User Name: $name.\n".
-              "User Email: $visitor_email.\n".
-              "Subect: $subject.\n".
-              "User Message: $message.\n";
+                "User Email": $subject.\n".
+                "Subject: $subject.\n".
+                "User Message: $message.\n";
+
 
 $to = 'sunilchandu18@gmail.com';
 
-$headers = "From: $email_from \r\n";
+$headers = "Form: $email_form \r\n";
 
-$headers .= "Reply-To: $visitor_email\r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
 
 mail($to,$email_subject,$email_body,$headers);
 
 header("Location: contact.html");
-
-
 ?>
